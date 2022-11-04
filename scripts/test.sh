b4f81b83d4967999e2491b420e3a3d0d7f3d3af8
Sebastián Ramírez <tiangolo@gmail.com>
2022-11-05 00:08:30 +0100

    👷 Move from pytest-cov to coverage and Codecov to Smokeshow (#483)

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ set -x
 
 bash ./scripts/test-files.sh
 # Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-pytest --cov=typer --cov=tests --cov=docs_src --cov-report=term-missing --cov-report=xml -o console_output_style=progress --forked --numprocesses=auto ${@}
+coverage run -m pytest -o console_output_style=progress --forked --numprocesses=auto ${@}
