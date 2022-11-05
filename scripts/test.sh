d96ee55c5f3feb1ecb485c1671f9bcc62d992001
Sebastián Ramírez <tiangolo@gmail.com>
2022-11-05 02:02:13 +0100

    ➕ Bring back pytest-cov because coverage can't detect pytest-xdist (#484)

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,4 +5,4 @@ set -x
 
 bash ./scripts/test-files.sh
 # Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-coverage run -m pytest -o console_output_style=progress --forked --numprocesses=auto ${@}
+pytest --cov=typer --cov=tests --cov=docs_src --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
