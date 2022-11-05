4fea6728cb179aee79902a50182e35a71c630aaa
Sebastián Ramírez <tiangolo@gmail.com>
2022-11-05 15:25:31 +0100

    🔧 Tweak pytest coverage (#485)

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 bash ./scripts/test-files.sh
-# Use xdist-pytest --forked to ensure modified sys.path to import relative modules in examples keeps working
-pytest --cov=typer --cov=tests --cov=docs_src --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
+# It seems xdist-pytest ensures modified sys.path to import relative modules in examples keeps working
+pytest --cov-config=.coveragerc --cov --cov-report=term-missing -o console_output_style=progress --numprocesses=auto ${@}
