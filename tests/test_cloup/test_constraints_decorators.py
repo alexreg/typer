a3578254f62c8583313fe15378985413b5a85ea5
Alexander Regueiro <alexreg@gmail.com>
2022-11-24 22:50:16 +0000

    ✨ Fix Flake8 issues in codebase

--- tests/test_cloup/test_constraints_decorators.py
+++ tests/test_cloup/test_constraints_decorators.py
@@ -1,4 +1,4 @@
-from typing import *
+from typing import Optional
 
 from cloup.constraints import RequireAtLeast, mutually_exclusive
 
--- tests/test_cloup/test_constraints_manual.py
+++ tests/test_cloup/test_constraints_manual.py
@@ -1,4 +1,4 @@
-from typing import *
+from typing import Optional
 
 from cloup.constraints import BoundConstraintSpec, RequireAtLeast, mutually_exclusive
 
